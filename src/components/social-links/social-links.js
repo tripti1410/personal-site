a15fec34c95df2f40d011b0d092ed16fce9224bd
Trapti Rahangdale <imtrapti1410@gmail.com>
2018-06-08 17:43:45 +0530

    fixed social share icons

--- src/components/social-links/social-links.js
+++ src/components/social-links/social-links.js
@@ -4,14 +4,17 @@ import "./social-links.css";
 const SocialLinks = () => {
   return (
     <div className="social-link-container">
-      <a href="https://twitter.com/imtraptir" className="social-link">
-        <i className="fab fa-twitter-square fa-2x" />
+      <a href="https://twitter.com/imtraptir" className="social-link" target="_blank">
+        <i className="fab fa-twitter" />
       </a>
-      <a href="https://github.com/tripti1410" className="social-link">
-        <i className="fab fa-github-square fa-2x" />
+      <a href="https://github.com/tripti1410" className="social-link" target="_blank">
+        <i className="fab fa-github" />
       </a>
-      <a href="www.linkedin.com/in/imtraptir" className="social-link">
-        <i className="fab fa-linkedin fa-2x" />
+      <a href="www.linkedin.com/in/imtraptir" className="social-link" target="_blank">
+        <i className="fab fa-linkedin-in" />
+      </a>
+      <a href="https://medium.com/@r_tripti" className="social-link" target="_blank">
+      <i class="fab fa-medium-m"></i>
       </a>
     </div>
   );
